2ce3320a89f174e65dfc712deaf8b196d9af75e9
Garkusha Valery <aston585v@gmail.com>
2024-05-19 17:17:06 +0300

    step #3 form user complit

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,9 +1,30 @@
 from django import forms
 from django.contrib.auth.views import get_user_model
-# from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinLengthValidator
 
 
 class UserCreateForm(forms.ModelForm):
+    password_min_len = 3
+
+    password1 = forms.CharField(
+        validators=[
+            MinLengthValidator(password_min_len,
+                               _("Password is too short"))],
+        label=_('Password'),
+        widget=forms.PasswordInput(),
+        help_text=_("Your password should be at least 3 symbols long."),
+    )
+
+    password2 = forms.CharField(
+        label=_('Password confirmation'),
+        validators=[
+            MinLengthValidator(password_min_len,
+                               _("Password is too short")),
+        ],
+        widget=forms.PasswordInput(),
+        help_text=_("Re-enter your password again, please."),
+    )
 
     class Meta:
         model = get_user_model()
