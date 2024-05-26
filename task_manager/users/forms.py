d1d06177dbea1c46628647ffef1c301f2bfd242e
Valeriy Garkusha <aston585v@gmail.com>
2024-05-26 15:15:35 +0500

    step #3 Add CRUD operations

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,35 +1,55 @@
 from django import forms
-from django.contrib.auth.views import get_user_model
+from django.contrib.auth.models import User as UserModel
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinLengthValidator
 
 
 class UserCreateForm(forms.ModelForm):
     password_min_len = 3
+    warning_password_min_len = _(
+        "The entered password is too short. "
+        "It must contain at least 3 characters."
+    )
 
     password1 = forms.CharField(
-        validators=[
-            MinLengthValidator(password_min_len,
-                               _("Password is too short"))],
         label=_('Password'),
         widget=forms.PasswordInput(),
-        help_text=_("Your password should be at least 3 symbols long."),
+        help_text=_(
+            "<ul>"
+            "<li>Your password must contain at least 3 characters.</li>"
+            "</ul>"
+        ),
     )
 
     password2 = forms.CharField(
         label=_('Password confirmation'),
         validators=[
             MinLengthValidator(password_min_len,
-                               _("Password is too short")),
+                               message=warning_password_min_len),
         ],
         widget=forms.PasswordInput(),
         help_text=_("Re-enter your password again, please."),
     )
 
     class Meta:
-        model = get_user_model()
+        model = UserModel
         fields = [
             "first_name",
             "last_name",
             "username",
         ]
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                _("Passwords don't match"), code='invalid')
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,26 +1,38 @@
 from task_manager.users.forms import UserCreateForm
-from django.contrib.auth.views import get_user_model
+from django.contrib.auth.models import User as UserModel
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-# from django.utils.translation import gettext_lazy as _
-# from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class ListUsersView(ListView):
-    model = get_user_model()
+    model = UserModel
     template_name = "users/users_list.html"
 
 
-class UserCreateView(CreateView):
-    model = get_user_model()
+class UserCreateView(SuccessMessageMixin, CreateView):
+    model = UserModel
     form_class = UserCreateForm
     template_name = 'users/create_user.html'
     success_url = reverse_lazy('home')
+    success_message = _('User successfully registered')
 
 
-class UserEditView(UpdateView):
-    pass
+class UserEditView(SuccessMessageMixin, UpdateView):
+    model = UserModel
+    form_class = UserCreateForm
+    template_name = 'users/update_user.html'
+    success_url = reverse_lazy("users:list_users")
+    success_message = _("User successfully changed")
+    failure_message = _("You do not have permission to change another user.")
+    failure_url = reverse_lazy("users:list_users")
 
 
-class UserDeleteView(DeleteView):
-    pass
+class UserDeleteView(SuccessMessageMixin, DeleteView):
+    model = UserModel
+    template_name = "users/delete_user.html"
+    success_url = reverse_lazy("users:list_users")
+    success_message = _('User deleted successfully')
+    failure_message = _("You do not have permission to change another user.")
+    failure_url = reverse_lazy('users:list_users')
