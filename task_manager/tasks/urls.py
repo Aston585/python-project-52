4472a8ddc11587e7f4c860b4949674f6d988934e
Valeriy Garkusha <aston585v@gmail.com>
2024-06-12 22:31:38 +0500

    step #5 Complit add views

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -5,15 +5,15 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.TasksListVew.as_view(), name='list_tasks'),
-    path('create/', views.TasksCreateView.as_view(), name='create_task'),
+    path('create/', views.TaskCreateView.as_view(), name='create_task'),
     path(
         '<int:pk>/update/',
-        views.TasksUpdateView.as_view(),
+        views.TaskUpdateView.as_view(),
         name='update_task'
     ),
     path(
         '<int:pk>/delete/',
-        views.TasksDeleteView.as_view(),
+        views.TaskDeleteView.as_view(),
         name='delete_task'
     ),
     path('<int:pk>/', views.TaskDetailView.as_view(), name='detail_task'),
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,10 +1,13 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from task_manager.mixins import LoginRequiredCustomMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from task_manager.tasks.models import Task
 from task_manager.tasks.forms import TaskForm
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 class TasksListVew(LoginRequiredCustomMixin, ListView):
@@ -12,7 +15,11 @@ class TasksListVew(LoginRequiredCustomMixin, ListView):
     model = Task
 
 
-class TasksCreateView(LoginRequiredCustomMixin, CreateView):
+class TaskCreateView(
+    LoginRequiredCustomMixin,
+    SuccessMessageMixin,
+    CreateView,
+):
     template_name = 'tasks/task_create.html'
     form_class = TaskForm
     success_url = reverse_lazy('tasks:list_tasks')
@@ -23,15 +30,45 @@ class TasksCreateView(LoginRequiredCustomMixin, CreateView):
         return super().form_valid(form)
 
 
-class TasksUpdateView(LoginRequiredCustomMixin, UpdateView):
-    pass
+class TaskUpdateView(
+    LoginRequiredCustomMixin,
+    SuccessMessageMixin,
+    UpdateView,
+):
+    model = Task
+    template_name = 'tasks/task_update.html'
+    form_class = TaskForm
+    success_url = reverse_lazy('tasks:list_tasks')
+    success_message = _('Task modified successfully')
 
 
-class TasksDeleteView(LoginRequiredCustomMixin, DeleteView):
+class TaskDeleteView(
+    LoginRequiredCustomMixin,
+    SuccessMessageMixin,
+    DeleteView,
+):
     template_name = 'tasks/task_delete.html'
     model = Task
     success_url = reverse_lazy('tasks:list_tasks')
+    success_message = _('Task deleted successfully')
+    error_message = _('Only its author can delete a task')
+
+    def get(self, request, *args, **kwargs):
+        curent_task = self.get_object()
+        if curent_task.author != request.user:
+            messages.error(self.request, self.error_message)
+            return redirect(self.success_url)
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.delete()
+        messages.success(self.request, self.success_message)
+        return redirect(success_url)
 
 
 class TaskDetailView(LoginRequiredCustomMixin, DetailView):
-    pass
+    template_name = 'tasks/task_detail.html'
+    model = Task
