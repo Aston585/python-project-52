5a81052627acc2c21466594ca9d45910922fcdae
Valeriy Garkusha <aston585v@gmail.com>
2024-06-18 10:32:01 +0500

    modified:   task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -162,7 +162,6 @@ FIXTURE_DIRS = [
 ROLLBAR = {
     'access_token': ROLLBAR_ACCESS_TOKEN,
     'environment': 'development' if DEBUG else 'production',
-    'code_version': '1.0',
     'root': BASE_DIR,
 }
 
